5474798c5f5ade99d7fea893a7b37425dab0a8cd
aden <aden.pribadi@gmail.com>
2026-02-28 17:28:10 +0700

    update script indexNow bing

--- scripts/indexnow.js
+++ scripts/indexnow.js
@@ -3,14 +3,7 @@ const https = require("https");
 const key = "a0bf1e37ff7349b99df98736ab383c0f";
 const host = "https://perbaikansaluranmampet.com";
 
-const urls = [
-  "/",
-  "/layanan",
-  "/blog",
-  "/kota/bandung",
-  "/kota/sukajadi",
-  "/kota/cicendo",
-];
+const urls = ["/"];
 
 const data = JSON.stringify({
   host: "perbaikansaluranmampet.com",
@@ -25,16 +18,20 @@ const options = {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
-    "Content-Length": data.length,
+    "Content-Length": Buffer.byteLength(data),
   },
 };
 
 const req = https.request(options, (res) => {
   console.log("IndexNow Status:", res.statusCode);
+
+  res.on("data", (chunk) => {
+    console.log("Response:", chunk.toString());
+  });
 });
 
 req.on("error", (error) => {
-  console.error(error);
+  console.error("IndexNow Error:", error);
 });
 
 req.write(data);
